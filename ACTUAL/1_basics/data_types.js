a9e50fe93ef67d547837d97a758db858f5ad84b7
seungit <minsg3669@gmail.com>
2023-09-24 17:02:08 +0900

    javascript basics study

---
+++ ACTUAL/1_basics/data_types.js
@@ -0,0 +1,43 @@
+/**
+ * Data types
+ * 
+ * 여섯개의 Primitive Typ과
+ * 한개의 오브젝트 타입이 있다.
+ * 
+ * 1) Number (숫자)
+ * 2) String (문자열)
+ * 3) Boolean (불리언)
+ * 4) undifined (언디파인드)
+ * 5) null (널)
+ * 6) Symbol (심볼)
+ * 
+ * 7) Object (객체)
+ *    Function
+ *    Array
+ *    Object
+ */
+
+const age = 30;
+const tempature = -10;
+const pi = 3.14;
+console.log(typeof age);  // number
+console.log(typeof tempature);  // number
+console.log(typeof pi);  // number
+console.log("--------------------");
+
+const infinity = Infinity;
+const nInfinity = -Infinity;
+console.log(typeof Infinity);  // number
+console.log(typeof -Infinity);  // number
+console.log("--------------------");
+
+/**
+ * String 타입
+ * 
+ */
+const seungit = 'seungit';
+console.log(seungit);
+
+const ive = "'아이브' 안유진";
+console.log(ive);
+
---
+++ ACTUAL/1_basics/naming_conventions.js
@@ -0,0 +1,18 @@
+/**
+ * naming_Conventions
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자의 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ * 4) 키워드는 변수명으로 사용 할 수 없다.
+ */
+
+let a = 'a';
+var $ive = 'b';
+const _yuJin = '안유진';
+
+console.log(a);
+console.log($ive);
+console.log(_yuJin);
+
+
